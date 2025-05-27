e4abcf197f91465a69a8cdbdbcbe7b25730db3fb
puslanMan <sy.renz.anthony@gmail.com>
2025-05-27 11:02:43 +0800

    started working on the development of front-end while backend server is live, practicing ci/cd as well in the process

--- backend/app.js
+++ backend/app.js
@@ -17,7 +17,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: (origin, callback) => {
+    // Allow all local development origins
+    callback(null, true);
+  },
+  credentials: true,
+}));
 
 app.use("/api/devices", deviceRouters);
 app.use("/api/users", userRouters);
