4bb7cd1de0dc7116d3e364ad3b7499b3f6c48c0f
puslanMan <sy.renz.anthony@gmail.com>
2025-06-17 04:49:23 +0800

    Alas! all pages are complete. The system is now ready to be fully deployed and be tested for data gathering and initial presentation

--- frontend/SmartBinAdmin/src/components/BarChartDashboard.jsx
+++ frontend/SmartBinAdmin/src/components/BarChartDashboard.jsx
@@ -6,10 +6,10 @@ const BarChartDashboard = ({data}) => {
     return (
         <div className="w-full max-w-2xl mx-auto mt-20">
 
-        <ResponsiveContainer width="80%" height={150}>
+        <ResponsiveContainer width="80%" height={230}>
             <BarChart data={data}>
-            <XAxis dataKey="value" />
-            <YAxis />
+            <XAxis dataKey="status" />
+            <YAxis allowDecimals={false} />
             <Tooltip />
             <Bar dataKey="value">
                 {data.map((_, index) => (
@@ -18,7 +18,7 @@ const BarChartDashboard = ({data}) => {
             </Bar>
             </BarChart>
         </ResponsiveContainer>
-        <h2 className="text-xl font-semibold mb-4 text-center text-gray-500">Number of Devices Online</h2>
+        <h2 className="text-xl font-semibold mb-4 text-center text-gray-500">Status Count of Devices</h2>
         </div>
     );
 }
--- frontend/SmartBinAdmin/src/pages/Home.jsx
+++ frontend/SmartBinAdmin/src/pages/Home.jsx
@@ -1,7 +1,11 @@
 import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import BasePage from '../components/BasePage';
 import axiosInstance from '../axiosConfig';
 import { toast } from 'react-toastify';
+
+import {MdSummarize} from 'react-icons/md'
+import {BsFillTrashFill} from 'react-icons/bs'
 
 import PieChartDashboard from '../components/PieChartDashboard';
 import BarChartDashboard from '../components/BarChartDashboard';
@@ -24,7 +28,6 @@ const Home = () => {
         if(!response2.data.success){
               toast.error(response2.data.message);
         }else{
-            console.log("data: "+JSON.stringify(response2.data.data));
             setDeviceStatusCount(response2.data.data);
         }
 
@@ -43,7 +46,17 @@ const Home = () => {
         <hr />
         <div className="w-full h-auto  grid grid-cols-2">
           <PieChartDashboard data={pieChartData}/>
-          <BarChartDashboard data={deviceStatusCount} />
+          <div className="flex flex-col w-full h-full">
+            <BarChartDashboard data={deviceStatusCount} />
+            <div className="flex flex-row w-full h-fit gap-10">
+              <Link to="/usages" className="button-in-use h-fit mt-10 flex gap-3">
+                <MdSummarize size="30"/> Check Usage Records
+              </Link>
+              <Link to="/devices" className="button-in-use h-fit mt-10 flex gap-3">
+                <BsFillTrashFill size="30"/> Check Devices
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     );
