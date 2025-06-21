d3bcefff9bc1070dd601949b0a35bb80a960d61e
puslanMan <sy.renz.anthony@gmail.com>
2025-06-21 22:50:15 +0800

    fixing some issues with the ui when the size changes

--- frontend/SmartBinAdmin/src/components/BasePage.jsx
+++ frontend/SmartBinAdmin/src/components/BasePage.jsx
@@ -38,7 +38,7 @@ const BasePage = ({pageTitle, setNextPage, pageContent, passwordModalReroute}) =
     <div className="flex w-screen min-h-screen">
       <Header pageTitle={pageTitle}/>
       <SideBar logoutButtonEventHandler={logOutButtonEventHandler} />
-      <div className="flex-1 min-h-screen h-full pb-40 bg-gray-100">
+      <div className="flex-1 min-h-screen h-full pb-40 bg-gray-100 w-full">
         { pageContent && pageContent(showPasswordModal) }
       </div>
     </div>
