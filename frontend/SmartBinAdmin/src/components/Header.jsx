3c21b9e9ee189920322c318cf19a12f1713f96c8
puslanMan <sy.renz.anthony@gmail.com>
2025-06-22 00:29:45 +0800

    fixing some issues with the ui when the size changes

--- frontend/SmartBinAdmin/src/components/Header.jsx
+++ frontend/SmartBinAdmin/src/components/Header.jsx
@@ -28,9 +28,9 @@ const Header = ({pageTitle}) => {
   }, []);
 
   return (
-    <div className='fixed top-0 left-25 w-full h-20 flex items-center bg-white shadow text-black z-10'>
-        <h1 className="mx-5 font-bold text-gray-800 text-2xl w-3/4">{pageTitle}</h1>
-        <span>Hi, {name}</span>
+    <div className='fixed top-0 left-25 w-full h-20 grid grid-cols-2 items-center bg-white shadow text-black z-10'>
+        <h1 className="mx-5 font-bold text-gray-800 text-2xl">{pageTitle}</h1>
+        <span className="text-center">Hi, {name}</span>
     </div>
   )
 }
